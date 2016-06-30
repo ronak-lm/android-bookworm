4436aa20cecccffb164c41400c6c72567f8c6c3a
Ronak Manglani <ronak.manglani@gmail.com>
2016-06-30 15:43:55 +0530

    Create singleton class for Volley

---
+++ app/src/main/java/com/ronakmanglani/booknerd/VolleySingleton.java
@@ -0,0 +1,42 @@
+package com.ronakmanglani.booknerd;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.support.v4.util.LruCache;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
+
+public class VolleySingleton {
+
+    // Singleton Instance
+    private static VolleySingleton instance;
+    public static VolleySingleton getInstance() {
+        if (instance == null) {
+            instance = new VolleySingleton();
+        }
+        return instance;
+    }
+
+    // Member objects
+    public RequestQueue requestQueue;
+    public ImageLoader imageLoader;
+
+    // Constructor
+    private VolleySingleton() {
+        requestQueue = Volley.newRequestQueue(BookNerdApplication.getAppContext());
+        imageLoader = new ImageLoader(requestQueue, new ImageLoader.ImageCache() {
+            private final LruCache<String, Bitmap> cache = new LruCache<>(20);
+            @Override
+            public Bitmap getBitmap(String url) {
+                return cache.get(url);
+            }
+            @Override
+            public void putBitmap(String url, Bitmap bitmap) {
+                cache.put(url, bitmap);
+            }
+        });
+    }
+
+}
