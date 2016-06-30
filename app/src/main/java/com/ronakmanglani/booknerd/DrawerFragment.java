ef9d8ac8b94664884577cfa9dd659514dffb5373
Ronak Manglani <ronak.manglani@gmail.com>
2016-06-30 11:33:49 +0530

    Complete drawer fragment

---
+++ app/src/main/java/com/ronakmanglani/booknerd/DrawerFragment.java
@@ -0,0 +1,61 @@
+package com.ronakmanglani.booknerd;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.NavigationView.OnNavigationItemSelectedListener;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+public class DrawerFragment extends Fragment implements OnNavigationItemSelectedListener {
+
+    private Unbinder unbinder;
+
+    @BindView(R.id.toolbar)         Toolbar toolbar;
+    @BindView(R.id.drawer_layout)   DrawerLayout drawerLayout;
+    @BindView(R.id.navigation_view) NavigationView navigationView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_drawer, container, false);
+        unbinder = ButterKnife.bind(this, v);
+        toolbar.setTitle(R.string.app_name);
+
+        // Setup navigation drawer
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(getActivity(), drawerLayout, toolbar, R.string.app_name, R.string.app_name) {
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+        };
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        navigationView.setNavigationItemSelectedListener(this);
+        actionBarDrawerToggle.syncState();
+
+        return v;
+    }
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        return false;
+    }
+}
