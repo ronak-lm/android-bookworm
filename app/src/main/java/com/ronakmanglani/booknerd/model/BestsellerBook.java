413919f4d527fef3e9f160219ce151dfeb004549
Ronak Manglani <ronak.manglani@gmail.com>
2016-07-01 17:41:56 +0530

    Create model class for bestseller books

---
+++ app/src/main/java/com/ronakmanglani/booknerd/model/BestsellerBook.java
@@ -0,0 +1,47 @@
+package com.ronakmanglani.booknerd.model;
+
+public class BestsellerBook {
+
+    // Attributes
+    private String title;
+    private String author;
+    private String description;
+    private String imageUrl;
+    private String weeksOnList;
+    private String isbn10;
+    private String isbn13;
+
+    // Getters
+    public String getTitle() {
+        return title;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public String getImageUrl() {
+        return imageUrl;
+    }
+    public String getWeeksOnList() {
+        return weeksOnList;
+    }
+    public String getIsbn10() {
+        return isbn10;
+    }
+    public String getIsbn13() {
+        return isbn13;
+    }
+
+    // Constructor
+    public BestsellerBook(String title, String author, String description, String imageUrl, String weeksOnList, String isbn10, String isbn13) {
+        this.title = title;
+        this.author = author;
+        this.description = description;
+        this.imageUrl = imageUrl;
+        this.weeksOnList = weeksOnList;
+        this.isbn10 = isbn10;
+        this.isbn13 = isbn13;
+    }
+}
