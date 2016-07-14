08e4a0fb18419116d8b881523e22739631cba358
Ronak Manglani <ronak.manglani@gmail.com>
2016-07-14 17:52:05 +0530

    Fix: Runtime camera permission for Marshmallow

--- app/src/main/java/com/ronakmanglani/booknerd/ui/fragment/DrawerFragment.java
+++ app/src/main/java/com/ronakmanglani/booknerd/ui/fragment/DrawerFragment.java
@@ -1,11 +1,15 @@
 package com.ronakmanglani.booknerd.ui.fragment;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.NavigationView.OnNavigationItemSelectedListener;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
@@ -28,6 +32,8 @@ import butterknife.ButterKnife;
 import butterknife.Unbinder;
 
 public class DrawerFragment extends Fragment implements OnNavigationItemSelectedListener {
+
+    private static final int CAMERA_REQUEST_CODE = 42;
 
     private Unbinder unbinder;
 
@@ -84,6 +90,20 @@ public class DrawerFragment extends Fragment implements OnNavigationItemSelected
         unbinder.unbind();
     }
 
+    // Handle permission requests for Marshmallow
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case CAMERA_REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startActivity(new Intent(getContext(), BarcodeActivity.class));
+                } else {
+                    Toast.makeText(getContext(), R.string.camera_permission_denied, Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+
     // Navigation drawer
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
@@ -96,7 +116,11 @@ public class DrawerFragment extends Fragment implements OnNavigationItemSelected
                 return false;
 
             case R.id.drawer_barcode:
-                startActivity(new Intent(getContext(), BarcodeActivity.class));
+                if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                   requestPermissions(new String[] { Manifest.permission.CAMERA }, CAMERA_REQUEST_CODE);
+                } else {
+                    startActivity(new Intent(getContext(), BarcodeActivity.class));
+                }
                 return false;
 
             case R.id.drawer_bestseller:
