67f07c904f87c84fdd377bf12a6c3d56650017ea
Ronak Manglani <ronak.manglani@gmail.com>
2016-07-22 20:41:10 +0530

    Use picasso to load image in BitmapUtil

--- app/src/main/java/com/ronakmanglani/bookworm/util/BitmapUtil.java
+++ app/src/main/java/com/ronakmanglani/bookworm/util/BitmapUtil.java
@@ -3,8 +3,11 @@ package com.ronakmanglani.bookworm.util;
 import android.content.Context;
 import android.content.ContextWrapper;
 import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 
 import com.ronakmanglani.bookworm.BookWormApp;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -14,26 +17,36 @@ public class BitmapUtil {
     private BitmapUtil() { }
 
     // Save Bitmap to file
-    public static void saveImageToStorage(final String bookId, final Bitmap bitmapImage){
+    public static void saveImageToStorage(final String bookId, final String imageUrl){
         // Save image in background thread
-        Runnable imageSaveTask = new Runnable() {
-            @Override
-            public void run() {
-                ContextWrapper cw = new ContextWrapper(BookWormApp.getAppContext());
-                // Get path to /data/data/your_app_package/app_data/images
-                File directory = cw.getDir("images", Context.MODE_PRIVATE);
-                File myPath = new File(directory, bookId + ".png");
-                // Save bitmap to storage
-                try {
-                    FileOutputStream fos = new FileOutputStream(myPath);
-                    bitmapImage.compress(Bitmap.CompressFormat.PNG, 100, fos);
-                    fos.close();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        };
-        new Thread(imageSaveTask).start();
+        Picasso.with(BookWormApp.getAppContext())
+                .load(imageUrl)
+                .into(new Target() {
+                    @Override
+                    public void onBitmapLoaded(final Bitmap bitmap, Picasso.LoadedFrom from) {
+                        new Thread(new Runnable() {
+                            @Override
+                            public void run() {
+                                ContextWrapper cw = new ContextWrapper(BookWormApp.getAppContext());
+                                // Get path to /data/data/your_app_package/app_data/images
+                                File directory = cw.getDir("images", Context.MODE_PRIVATE);
+                                File myPath = new File(directory, bookId + ".png");
+                                // Save bitmap to storage
+                                try {
+                                    FileOutputStream fos = new FileOutputStream(myPath);
+                                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos);
+                                    fos.close();
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }).start();
+                    }
+                    @Override
+                    public void onBitmapFailed(Drawable errorDrawable) { }
+                    @Override
+                    public void onPrepareLoad(Drawable placeHolderDrawable) { }
+                });
     }
 
     // Get image file from book ID
