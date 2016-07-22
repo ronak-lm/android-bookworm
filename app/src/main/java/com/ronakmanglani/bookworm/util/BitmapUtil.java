b434647e4ae4515928e831636b44478114da5901
Ronak Manglani <ronak.manglani@gmail.com>
2016-07-22 20:01:59 +0530

    Create BitmapUtil to save/load image

---
+++ app/src/main/java/com/ronakmanglani/bookworm/util/BitmapUtil.java
@@ -0,0 +1,43 @@
+package com.ronakmanglani.bookworm.util;
+
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.graphics.Bitmap;
+
+import com.ronakmanglani.bookworm.BookWormApp;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+public class BitmapUtil {
+
+    // Save Bitmap to file
+    private void saveToInternalStorage(final String bookId, final Bitmap bitmapImage){
+        // Save image in background thread
+        Runnable imageSaveTask = new Runnable() {
+            @Override
+            public void run() {
+                ContextWrapper cw = new ContextWrapper(BookWormApp.getAppContext());
+                // Get path to /data/data/your_app_package/app_data/images
+                File directory = cw.getDir("images", Context.MODE_PRIVATE);
+                File myPath = new File(directory, bookId + ".png");
+                // Save bitmap to storage
+                try {
+                    FileOutputStream fos = new FileOutputStream(myPath);
+                    bitmapImage.compress(Bitmap.CompressFormat.PNG, 100, fos);
+                    fos.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        new Thread(imageSaveTask).start();
+    }
+
+    // Get image file from book ID
+    private File loadImageFromStorage(String bookId) {
+        ContextWrapper cw = new ContextWrapper(BookWormApp.getAppContext());
+        File directory = cw.getDir("images", Context.MODE_PRIVATE);
+        return new File(directory, bookId + ".png");
+    }
+}
