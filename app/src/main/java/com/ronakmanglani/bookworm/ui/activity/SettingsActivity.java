1ef3df42ff1c72482ef64a4072d9f4c71e2d20c3
Ronak Manglani <ronak.manglani@gmail.com>
2016-07-25 17:41:59 +0530

    Create SettingsActivity to show SettingsFragment

---
+++ app/src/main/java/com/ronakmanglani/bookworm/ui/activity/SettingsActivity.java
@@ -0,0 +1,34 @@
+package com.ronakmanglani.bookworm.ui.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import com.ronakmanglani.bookworm.R;
+import com.ronakmanglani.bookworm.ui.fragment.SettingsFragment;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        getFragmentManager().beginTransaction().replace(R.id.settings_layout, new SettingsFragment()).commit();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
